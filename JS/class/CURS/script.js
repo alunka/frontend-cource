513d5e918b4d878e5171f9c4eb5c600605c1827b
Alla Jakymchuk <allayakimchyk.1990@gmail.com>
2023-04-30 13:38:48 +0300

    Finalized currency converter for privatebank

--- JS/class/CURS/script.js
+++ JS/class/CURS/script.js
@@ -1,10 +1,31 @@
-const url = https://api.privatbank.ua/p24api/exchange_rates?date=01.12.2014
+async function privatbank () {
+    const url = 'https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5';
 
-async function privatbank (url) {
-    try {
-        const res = await fetch (url)
-        const data = await res.json()
-        console.log(res);
-        console.log(data);
-    }
+    await fetch(url , {
+            method: 'GET'
+        }).then(response => {
+            if (response.ok) return response.json();
+            throw new Error("Error fetching data");
+        })
+        .then(data => {
+            data.forEach((currency) => {
+                let buyCell = document.getElementById(`${currency.ccy}_buy`);
+                let saleCell = document.getElementById(`${currency.ccy}_sale`);
+                if(buyCell !== undefined && buyCell !== null) {
+                    buyCell.innerHTML = currency.buy;
+                    document.getElementById(`${currency.ccy}_buy_amount`).innerHTML =
+                    (currency.buy * document.getElementById('currency_amount').value).toFixed(2) + ' ₴';
+                }
+                if(saleCell !== undefined && saleCell !== null) {
+                    saleCell.innerHTML = currency.sale;
+                    document.getElementById(`${currency.ccy}_sale_amount`).innerHTML =
+                    (currency.sale * document.getElementById('currency_amount').value).toFixed(2) + ' ₴';
+                }
+            });
+        })
+        .catch(err => {
+            console.error("Error: ", err);
+        });
 }
+
+privatbank();
